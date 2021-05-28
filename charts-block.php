efdfca9441a47acaa9845653e444b3ce6ce29b91
thisissandip <thisissandipmondal@gmail.com>
2021-05-28 11:14:32 +0530

    Version 1.0.0

--- charts-block.php
+++ charts-block.php
@@ -3,62 +3,63 @@
 /**
  * Plugin Name: Charts Blocks
  * Description: Responsive, Interactive and Animated Charts in just two simple clicks. Select a Chart Block, Upload your CSV file and DONE. It's that easy!
- * Version: 1.0
+ * Version: 1.0.0
  * Author: Sandip Mondal
  *
  * @category Gutenberg
  * @author Sandip Mondal
- * @version 1.0
+ * @version 1.0.0
  */
 
 if(!defined("ABSPATH")) {
     exit;
 };
 
-define('CHARTS_BLOCKS_VERSION',"1.0");
+define('CHARTS_BLOCKS_VERSION',"1.0.0");
 define('CHARTS_BLOCKS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) ) ;
 define('CHARTS_BLOCKS_PLUGIN_URL',  plugin_dir_url( __FILE__ )) ;
-define('CHARTS_BLOCKS_BASE', plugin_basename( __FILE__ ) );
+define('CHARTS_BLOCKS_BASE', plugin_basename( __FILE__ ) ) ;
 
-function load_charts_block(){
+if( ! function_exists("sandip_charts_blocks_load_all_blocks_assets")){
 
-    $slug = "charts-blocks";
-    $assets = include(CHARTS_BLOCKS_PLUGIN_PATH . 'build/index.asset.php'); 
+    function sandip_charts_blocks_load_all_blocks_assets(){
+        $slug = "charts-blocks";
+        $assets = include(CHARTS_BLOCKS_PLUGIN_PATH . 'build/index.asset.php'); 
+        
+        // enqueue chartjs script for frontend
+        wp_enqueue_script($slug.'-chartjs',CHARTS_BLOCKS_PLUGIN_URL . "chartjs/chart.min.js", array(), "1.0", false);
+        // enqueue block assets
+        wp_register_script( $slug.'-script' , CHARTS_BLOCKS_PLUGIN_URL . 'build/index.js',
+        $assets["dependencies"],  $assets["version"], false);
+        wp_enqueue_script($slug.'-script');
+         // provide site url to editor script
+        wp_add_inline_script( $slug.'-script', 'let siteurl = "'.CHARTS_BLOCKS_PLUGIN_URL.'"', "before" );
     
-    // enqueue chartjs script for frontend
-    wp_enqueue_script("chartjs",CHARTS_BLOCKS_PLUGIN_URL . "chartjs/chart.min.js", array(), "1.0", false);
-   
-    // enqueue block assets
-    wp_register_script( $slug.'-script' , CHARTS_BLOCKS_PLUGIN_URL . 'build/index.js',
-    $assets["dependencies"],  $assets["version"], false);
-    wp_enqueue_script($slug.'-script');
-     // provide site url to editor script
-    wp_add_inline_script( $slug.'-script', 'let siteurl = "'.CHARTS_BLOCKS_PLUGIN_URL.'"', "before" );
+        wp_enqueue_script($slug.'-frontendscript',CHARTS_BLOCKS_PLUGIN_URL ."build/frontendscript.js", array(), 1.0 ,true);
+        // variable to store all charts attributes
+        wp_add_inline_script( $slug.'-frontendscript', 'let allchartsdata = []' , "before" );
+    
+        $all_blocks = array("bar","pie","doughnut", "line","horizontal-bar","horizontal-line");
+        // register all blocks in the array
+        foreach($all_blocks as $block_name ){
+            sandip_charts_blocks_register_blocks($slug, $block_name);
+        }  
+    }
 
-    wp_enqueue_script($slug.'-frontendscript',CHARTS_BLOCKS_PLUGIN_URL ."build/frontendscript.js", array(),1.0 ,true);
-    // variable to store all charts attributes
-    wp_add_inline_script( "charts-blocks-frontendscript", 'let allchartsdata = []' , "before" );
-
-    $all_blocks = array("bar","pie","doughnut", "line","horizontal-bar","horizontal-line");
-    // register all blocks in the array
-    foreach($all_blocks as $block_name ){
-        register_blocks($slug, $block_name);
-    }  
 }
 
-// Front End HTML
 
-function display_chart($attributes){
+// Render Callback Function
+
+function sandip_charts_blocks_render_all_charts($attributes){
         ob_start(); 
-
         $chartid = $attributes["chartID"] ? $attributes["chartID"] : "chart";
         wp_add_inline_script( "charts-blocks-frontendscript", 'allchartsdata.push( '. json_encode($attributes).')' , "before" );
         $alignWidth = $attributes["align"] ? $attributes["align"] : null;
 
         $alignClass = $attributes["align"] ? ' align'.$attributes["align"] : null;
         $maxWidth  = $alignClass ? null : "style='max-width: 600px'";
-        
-/*         echo "<pre>";
+        /*  echo "<pre>";
             print_r($attributes);
         echo "</pre>"; */ 
 
@@ -76,44 +77,52 @@ function display_chart($attributes){
         /* HTML OUTPUT END */
 
         return ob_get_clean();
-     
 }
 
-function register_blocks($slug, $block_name){
-    register_block_type( $slug . '/' . $block_name, array(
-        "editor_script" => $slug . '-script',
-        "script"        => $slug . '-frontendscript',
-        'attributes'      => array( 
-            'chartType' => array(
-                "type" => "string",
-                "default" => $block_name
-            ),
-        ), 
-        "render_callback" => "display_chart"
-    ) );
-} 
 
-add_action("init", "load_charts_block");
+// Register Blocks
 
-// Add new Block Categorie - charts_blocks
-
-function charts_blocks_categories($categories)
-{
-    $existing_categories = wp_list_pluck( $categories, 'slug' );
-    
-    if( ! in_array("charts_block_sandip", $categories))
-    { 
-        $new_block_categories = array_merge($categories, array(
-            array(
-                "slug" => "charts-blocks",
-                "title" => "Charts",
-                "icons" => NULL
-            )
-            ));
-                
-        return $new_block_categories;
+if( ! function_exists("sandip_charts_blocks_register_blocks")){
+    function sandip_charts_blocks_register_blocks($slug, $block_name){
+        register_block_type( $slug . '/' . $block_name, array(
+            "editor_script" => $slug . '-script',
+            "script"        => $slug . '-frontendscript',
+            'attributes'      => array( 
+                'chartType' => array(
+                    "type" => "string",
+                    "default" => $block_name
+                ),
+            ), 
+            "render_callback" => "sandip_charts_blocks_render_all_charts"
+        ) );
     } 
-
 }
 
-add_filter("block_categories", "charts_blocks_categories");
+add_action("init", "sandip_charts_blocks_load_all_blocks_assets");
+
+
+// Add new Block Category - charts_blocks
+
+if( ! function_exists("sandip_register_charts_blocks_category")){
+
+    function sandip_register_charts_blocks_category($categories)
+    {
+        $existing_categories = wp_list_pluck( $categories, 'slug' );
+        
+        if( ! in_array("charts-blocks", $categories))
+        { 
+            $new_block_categories = array_merge($categories, array(
+                array(
+                    "slug" => "charts-blocks",
+                    "title" => "Charts",
+                    "icons" => NULL
+                )
+                ));
+                    
+            return $new_block_categories;
+        } 
+    }
+    
+}
+
+add_filter("block_categories", "sandip_register_charts_blocks_category");
---
+++ readme.txt
@@ -0,0 +1,55 @@
+=== Charts Blocks ===
+
+Contributors: sandipmondal
+Tags: charts, graphs, charts blocks, graph blocks, data visualiztion
+Requires at least: 5.0
+Tested up to: 5.7
+Stable tag: 1.0.0
+Requires PHP: 7.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Responsive, Interactive and Animated Charts in just two simple clicks. Select a Chart Block, Upload your CSV file and DONE. It's that easy!
+
+== Description ==
+
+### The Easiest way to create responsive, interactive and animated charts in Wordpress.
+
+Select a Chart Block from the availabe chart types. Currently there are 6 different Chart Blocks: 
+
+1. Bar Chart Block
+2. Line Chart Block
+3. Pie Chart Block
+4. Doughnut Chart Block
+5. Horizontal Bar Chart Block
+6. Horizontal Line Chart Block
+
+Upload your CSV file and TADAA! 
+
+Your Beautiful, Animated and Responsive chart is ready for the world!
+
+== Installation ==
+
+1. Log in to your WordPress dashboard
+2. Visit Plugins > Add New
+3. Search for Charts Blocks
+4. Install and Activate
+
+== Frequently Asked Questions ==
+
+= Is Charts Blocks Free =
+
+Yes, Charts Blocks is 100% Free.
+
+= How do I create my first Chart =
+
+Navigate to the Page or Post where you want to add a chart. Select one Chart Block from the available chart blocks. Upload your CSV file. Your beautiful chart is ready for the world.
+
+= Why is my CSV data not showing in Charts =
+
+Download the example CSV file provided below the Upload CSV button. Make sure your data is in the specified format.
+
+= How can I add Grouped Bar Charts or Multiline Charts =
+
+Grouped Bar Charts, Multiline Charts and many more Chart Types will be added in the upcoming versions.
+
